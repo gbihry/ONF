33b90b04b725dc5218fa6b6896dba3133348fd2c
gbihry <98669194+gbihry@users.noreply.github.com>
2023-01-19 16:09:06 +0100

    Change title catalogue + regle associé + css

--- vue/vueCatalogueVet.php
+++ vue/vueCatalogueVet.php
@@ -1,4 +1,7 @@
 <div class="catalogue">
+    <div class="text-center">
+        <p class="catalogue_title_type"> Catalogue VET</p>
+    </div>
     <div class="contenue">
     <?php 
         foreach($catalogue as $uneCategorie){
