716b8a1f3a7ef5b2f3f01e268496074ba51a64ed
gbihry <bihryguillaume@gmail.com>
2023-03-20 14:42:29 +0100

    todolist

--- todo.php
+++ todo.php
@@ -16,41 +16,9 @@
 
 // ? Tristan  
 
-
-//TODO : Envoie d'un e-mail si les utilisateurs ont pas encore valider le panier ou commander
-
-//TODO -image de fond sur la page d'acceuil 
-
-//TODO -onf , commune forestier logo un peu plus grand 
-
-//TODO -pouvoir changer le métier d'une personne 
-
-//TODO -tant qu'il n'a  pas passer de commande on peut changer les info des utilisateur sur la page add user 
-
-//TODO -pour les ouvrier toujours  a muhouse le site de livraison 
-
 //TODO -xs court , xs stantard , xs long pour les pantalon anticoupurs , spb. 
 
-//TODO -commande subordonnées = crée un pdf avec toutes les commandes de l'équipe individuellement  
-
-//TODO -remplacer le mdp par le numéro de téléphone 
-
-//TODO -dans le pdf récapitulatif rajouter les tailles  
-
 //TODO -Admin = gestionnaire de commandes qui peut s'occuper de toute son agence 
-
-//TODO -produits EPI = Produits EPI Ouvrier 
-
-//TODO -csv par site et par fournisseur  
-//TODO -dissocier élément chaussant et pantalon (rajouter une nouvelle catégories )
-
-//TODO -bucheron = que SIP 1SPV Sinon sylviculteur le reste des pantalons 
-
-//TODO -nom de l'article + nom commerciale 
-
-//TODO -1SX4 enlever pour le bucheron 
-
-//TODO -enlever les casque et equipement jetables pour les bucheron 
 
 //TODO -rajouter type pour les lunettes a la place de taille : transparant , jaune ou fumée  ( 1 paires maximum) 
 
@@ -64,13 +32,9 @@
 
 //TODO -DENIM = chauffeur d'engin et logisticien et débardeur 
 
-//TODO -crée un métier débardeur
-
-//TODO -vet tous accès 
+//TODO -crée un métier débardeur 
 
 //TODO -dans le texte mettre en gras les PS ( déscription produit ) 
-
-//TODO -enlever les points à ce qui non pas accès au catalogue VET
 
 //TODO -rajouter des catégories au EPI Non Ouvrier
 
@@ -81,17 +45,5 @@
 //TODO -rajouter cartouche en produits en dessous de MSA Advantage 200 LS 
 
 //TODO -" votre panier sera valider par votre supérieur, toute commande est définitif ", son responsable pourra supprimer la commande validée au cas ou la commande n'es pas conforme et refaite par le responsable
-//TODO l'utilisateur n'auras plus accès aux catalogues
 
-//TODO -pas de quantité limite pour les cagoules YVEK 500 dans catalogues Non Ouvrier 
-
-//TODO -mettre dans l'entete point = 0 et rajouter texte " point restant perdu " quand commande passer
-
-
-
-
-
-
-
-//TODO -mettre dans l'entete point = 0 et rajouter texte " point restant perdu " quand commande passer
 //TODO -pas de quantité limite pour les cagoules YVEK 500 dans catalogues Non Ouvrier 
