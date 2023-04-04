3b3d1b33cd7075ff053af53d3c37cbb41c4211f4
MatthieuRoess <123086572+MatthieuRoess@users.noreply.github.com>
2023-04-04 09:27:09 +0200

    log ajout points

--- controleur/ctrlAjoutPoint.php
+++ controleur/ctrlAjoutPoint.php
@@ -9,6 +9,13 @@
             $idUtilisateur = $_POST['user'];
             $points = $_POST['nombrepoint'];
             ModeleObjetDAO::insertPoints($idUtilisateur, $points);
+
+            date_default_timezone_set('Europe/Paris');
+            $login = ModeleObjetDAO::getLoginById($idUtilisateur);
+            $description = "Ajout de points à ".$login["login"]." par ".$_SESSION['login'];
+            $idChef = ModeleObjetDAO::getIdUtilisateur($_SESSION['login']);
+            $date = date( "Y-m-d H:i:s");
+            ModeleObjetDAO::insertLog($date,$description,$idChef["id"]);
             $reload = true;
         }
     } else {
